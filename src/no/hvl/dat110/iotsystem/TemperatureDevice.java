633c18d73e174d3b41374b7137db2e8fc62893f8
h131177 <131177@stud.hvl.no>
2020-03-11 12:05:10 +0100

    TemperatureDevice

--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -15,16 +15,23 @@ public class TemperatureDevice {
 		// TODO - start
 
 		// create a client object and use it to
+		Client client = new Client("TemperatureDevice", Common.BROKERHOST, Common.BROKERPORT);
 
 		// - connect to the broker
+		client.connect();
+		
 		// - publish the temperature(s)
+		for(int i = 0; i < COUNT; i++) {
+			client.publish(Common.TEMPTOPIC, String.valueOf(sn.read()));
+		}
 		// - disconnect from the broker
+		client.disconnect();
 
 		// TODO - end
 
 		System.out.println("Temperature device stopping ... ");
 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
